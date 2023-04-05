db17f6e318e76294391a2126f94868b4f0cf52a3
Yash Bansal <yashbansal97@gmail.com>
2023-04-05 23:37:19 +0530

    more content

--- src/App.tsx
+++ src/App.tsx
@@ -60,7 +60,7 @@ const App: React.FC = () => {
         <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
           <div style={{ padding: 24, textAlign: 'center', background: colorBgContainer }}>
             <p>The quick brown fox jumps over lazy little dog</p>
-            {/* {
+            {
               // indicates very long content
               Array.from({ length: 10 }, (_, index) => (
                 <React.Fragment key={index}>
@@ -68,7 +68,7 @@ const App: React.FC = () => {
                   <br />
                 </React.Fragment>
               ))
-            } */}
+            }
           </div>
         </Content>
         <Footer style={{ textAlign: 'center' }}>Yash Bansal ©2023</Footer>
