4a723001f4ba6158f2fae7439a8fe91b62a5a267
Yash Bansal <yashbansal97@gmail.com>
2023-04-08 23:22:32 +0530

    new input box

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,10 @@ import {
   VideoCameraOutlined,
 } from '@ant-design/icons';
 import type { MenuProps } from 'antd';
-import { Layout, Menu, theme, Typography } from 'antd';
+import { Layout, Menu, theme, Typography, Input } from 'antd';
 import { GiMountaintop, GiBoxingRing } from 'react-icons/gi';
 const { Title } = Typography;
+const { TextArea } = Input;
 const { Header, Content, Footer, Sider } = Layout;
 
 const items: MenuProps['items'] = [
@@ -55,22 +56,23 @@ const App: React.FC = () => {
         <Title style={{ margin: "10px 0", color: 'white', display: "flex", justifyContent: "center" }} level={4}>Fight Club GPT</Title>
         <Menu theme="dark" style={{ background: 'green'}} mode="inline" defaultSelectedKeys={['4']} items={items} />
       </Sider>
-      <Layout className="site-layout" style={{ marginLeft: 200 }}>
-        <Header style={{ padding: 0, background: colorBgContainer }} />
-        <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+      <Layout className="site-layout" style={{ marginLeft: 200, padding: 50}}>
+        {/* <Header style={{ padding: 0, background: colorBgContainer }} /> */}
+        <TextArea rows={4} placeholder="Type your question" maxLength={6} />
+        {/* <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
           <div style={{ padding: 24, textAlign: 'center', background: colorBgContainer }}>
             {
               // indicates very long content
               Array.from({ length: 10 }, (_, index) => (
                 <React.Fragment key={index}>
-                  {index % 10 === 0 && index ? 'more' : '...'}
+                  {index % 2 === 1 && index ? 'more' : '...'}
                   <p>The quick brown fox jumps over lazy little dog</p>
                   <br />
                 </React.Fragment>
               ))
             }
           </div>
-        </Content>
+        </Content> */}
         <Footer style={{ textAlign: 'center' }}>Yash Bansal ©2023</Footer>
       </Layout>
     </Layout>
